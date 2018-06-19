2af66110e83441ac896c0229eae5e2d28579f986
maktag <maktagstudy@gmail.com>
2018-06-19 19:46:35 +0530

    comment…….

--- trial/Demo.py
+++ trial/Demo.py
@@ -6,18 +6,20 @@ class Test_suit(unittest.TestCase):
     st = status()
 
     def test_1(self):
-        self.st.pass_test('001','Pass with a and b user.')
+        self.st.pass_test('001', 'Pass with pradeep','T')
+        self.st.fail_test('002', 'Fail with anil')
+        self.st.error_test('003', 'Error with ankur')
+        self.st.skip_test('004', 'Skip with ankit')
+        self.st.info_test('005', 'Info with sachin')
+        self.st.create_module_dict('Login With Facebook')
 
     def test_2(self):
-        self.st.pass_test('002','Pass with a and b user.')
-
-    def test_3(self):
-        self.st.pass_test('002','Pass with a and b user.')
-
-    def test_4(self):
-        self.st.create_module_dict('Login With Twitter')
-        print("This is Three: ", self.st.test_unit)
-
+        self.st.pass_test('006', 'Pass with pradeep','T')
+        self.st.fail_test('007', 'Fail with anil')
+    #     self.st.error_test('003', 'Error with ankur')
+    #     self.st.skip_test('004', 'Skip with ankit')
+    #     self.st.info_test('005', 'Info with sachin')
+        self.st.create_module_dict('Login With Twiter')
 
 if __name__ == '__main__':
     unittest.main()
--- trial/Status.py
+++ trial/Status.py
@@ -1,45 +1,58 @@
+from trial.createHtml import Prepare_report
+import pyscreenshot as ImageGrab
+import os
+
 
 class status:
 
+    def __init__(self):
+        if not os.path.exists('test_report'):
+            os.makedirs('test_report')
+        if not os.path.exists('test_report/images'):
+            os.makedirs('test_report/images')
+
     test_unit = {}
-    pass_case = {}
-    fail_case = {}
-    error_case = {}
-    skip_case = {}
-    info_case = {}
+    test_case_with_status = {}
+    test_modules_name = []
 
-    def pass_test(self, tc_id, tc_Message):
-        self.pass_case[tc_id] = tc_Message
-        return self.pass_case
+    def pass_test(self, tc_id, tc_Message, *save_screenshot):
+        self.test_case_with_status[tc_id] = tc_Message+'P'
+        for ar in save_screenshot:
+            if ar == 'T':
+                ImageGrab.grab_to_file('test_report/images/'+tc_id+'.png')
+        return self.test_case_with_status
 
-    def fail_test(self, tc_id, tc_Message):
-        self.fail_case[tc_id] = tc_Message
-        return self.fail_case
+    def fail_test(self, tc_id, tc_Message, *save_screenshot):
+        self.test_case_with_status[tc_id] = tc_Message+'F'
+        for ar in save_screenshot:
+            if ar == 'T':
+                ImageGrab.grab_to_file('test_report/images/' + tc_id + '.png')
+        return self.test_case_with_status
 
-    def error_test(self, tc_id, tc_Message):
-        self.error_case[tc_id] = tc_Message
-        return self.error_case
+    def error_test(self, tc_id, tc_Message, *save_screenshot):
+        self.test_case_with_status[tc_id] = tc_Message+'E'
+        for ar in save_screenshot:
+            if ar == 'T':
+                ImageGrab.grab_to_file('test_report/images/' + tc_id + '.png')
+        return self.test_case_with_status
 
-    def skip_test(self, tc_id, tc_Message):
-        self.skip_case[tc_id] = tc_Message
-        return self.skip_case
+    def skip_test(self, tc_id, tc_Message, *save_screenshot):
+        self.test_case_with_status[tc_id] = tc_Message+'S'
+        for ar in save_screenshot:
+            if ar == 'T':
+                ImageGrab.grab_to_file('test_report/images/' + tc_id + '.png')
+        return self.test_case_with_status
 
-    def info_test(self, tc_id, tc_Message):
-        self.info_case[tc_id] = tc_Message
-        return self.info_case
+    def info_test(self, tc_id, tc_Message, *save_screenshot):
+        self.test_case_with_status[tc_id] = tc_Message+'I'
+        for ar in save_screenshot:
+            if ar == 'T':
+                ImageGrab.grab_to_file('test_report/images/' + tc_id + '.png')
+        return self.test_case_with_status
 
     def create_module_dict(self, ModuleName):
+        self.test_modules_name.append(ModuleName)
         self.test_unit['Module_Name'] = ModuleName
-        self.test_unit['Pass_Cases'] = self.pass_case
-        self.test_unit['Fail_Cases'] = self.fail_case
-        self.test_unit['Error_Cases'] = self.error_case
-        self.test_unit['Skip_Cases'] = self.skip_case
-        self.test_unit['Info_Cases'] = self.info_case
-
-
-# st = status()
-# st.pass_test('001','Message')
-# st.fail_test('001','Message')
-# st.error_test('001','Message')
-# st.create_module_dict('Login')
-# print(st.test_unit)
+        self.test_unit['All_Cases'] = self.test_case_with_status
+        print(self.test_unit)
+        Prepare_report(self.test_unit,self.test_modules_name,ModuleName)
